6d7e55b1ac427fec7dc00c70896e95d5d42ac00c
Dennis Fedorov <115027699+DennisUser1@users.noreply.github.com>
2025-02-18 17:56:57 +0200

    v.1.5.  Added creating, deleting tasks

--- src/pages/todo-list/page.tsx
+++ src/pages/todo-list/page.tsx
@@ -1,7 +1,15 @@
-import { Suspense, useState } from "react";
+import {
+  startTransition,
+  Suspense,
+  use,
+  useActionState,
+  useMemo,
+  useState,
+} from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import { fetchTasks, Task } from "../../shared/api";
 import { useParams } from "react-router-dom";
+import { createTaskAction, deleteTaskAction } from "./action";
 
 function ErrorFallback({ error }: { readonly error: unknown }) {
   const errorMessage =
@@ -12,34 +20,57 @@ function ErrorFallback({ error }: { readonly error: unknown }) {
 }
 
 export function TodoListPage() {
-  const { userId }= useParams();
-  const [tasksPromise, setTasksPromise] = useState(() => fetchTasks({ filters: {userId} }));
+  const { userId = "" } = useParams();
+  const [paginatedTasksPromise, setPaginatedTasksPromise] = useState(() =>
+    fetchTasks({ filters: { userId } })
+  );
+  const refetchTasks = () =>
+    startTransition(() =>
+      setPaginatedTasksPromise(fetchTasks({ filters: { userId } }))
+    );
+
+  const tasksPromise = useMemo(
+    () => paginatedTasksPromise.then((r) => r.data),
+    [paginatedTasksPromise]
+  );
 
   return (
     <main className="container mx-auto p-4 pt-10 flex flex-col gap-4">
       <h1 className="text-3xl font-bold underline">Tasks: user {userId}</h1>
-      <CreateTaskForm />
+      <CreateTaskForm refetchTasks={refetchTasks} userId={userId} />
       <ErrorBoundary fallbackRender={ErrorFallback}>
         <Suspense fallback={<div>Loading...</div>}>
-          <TasksList />
+          <TasksList tasksPromise={tasksPromise} refetchTasks={refetchTasks}/>
         </Suspense>
       </ErrorBoundary>
     </main>
   );
 }
 
-export function CreateTaskForm() {
-  return (
-    <form className="flex gap-2 items-start">
-      <div className="flex flex-col">
-        <input name="email" type="email" className="border p-2 rounded" />
+export function CreateTaskForm({
+  userId,
+  refetchTasks,
+}: {
+  readonly userId: string;
+  readonly refetchTasks: () => void;
+}) {
+  const [state, dispatch, isPending] = useActionState(
+    createTaskAction({ refetchTasks, userId }),
+    { title: "" }
+  );
 
-        <div className="text-red-500 mt-1">error</div>
+  return (
+    <form className="flex gap-2 items-start" action={dispatch}>
+      <div className="flex flex-col">
+        <input name="title" type="text" className="border p-2 rounded" />
+        {state.error && <div className="text-red-500 mt-1">{state.error}</div>}
       </div>
       <div>
         <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:bg-gray-400"
           type="submit"
+          defaultValue={state.title}
+          disabled={isPending}
         >
           Add
         </button>
@@ -48,27 +79,51 @@ export function CreateTaskForm() {
   );
 }
 
-export function TasksList() {
-  const tasks = [] as Task[];
+export function TasksList({
+  tasksPromise,
+  refetchTasks,
+}: {
+  readonly tasksPromise: Promise<Task[]>;
+  readonly refetchTasks: () => void;
+}) {
+  const tasks = use(tasksPromise);
   return (
     <div className="flex flex-col gap-2">
       {tasks.map((task) => (
-        <TaskCard key={task.id} task={task} />
+        <TaskCard key={task.id} task={task} refetchTasks={refetchTasks} />
       ))}
     </div>
   );
 }
 
-export function TaskCard({ task }: { readonly task: Task }) {
+export function TaskCard({
+  task,
+  refetchTasks,
+}: {
+  readonly task: Task;
+  readonly refetchTasks: () => void;
+}) {
+  const [deleteState, handleDelete, isPending] = useActionState(
+    deleteTaskAction({ refetchTasks }),
+    {}
+  );
   return (
-    <div className="border p-2 rounded bg-gray-100 flex gap-2 items-center">
-      {task.title}
-      <form className="ml-auto">
-        <input type="hidden" name="id" value={task.id} />
-        <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-auto">
-          Delete{" "}
-        </button>
-      </form>
-    </div>
+    <>
+      <div className="border p-2 rounded bg-gray-100 flex gap-2 items-center">
+        {task.title}
+        <form className="ml-auto" action={handleDelete}>
+          <input type="hidden" name="id" value={task.id} />
+          <button
+            disabled={isPending}
+            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-auto disabled:bg-gray-400"
+          >
+            Delete{" "}
+          </button>
+        </form>
+      </div>
+      {deleteState.error && (
+        <div className="text-red-500">{deleteState.error}</div>
+      )}
+    </>
   );
 }
--- src/shared/api.ts
+++ src/shared/api.ts
@@ -37,8 +37,8 @@ export type Task = {
   id: string;
   userId: string;
   title: string;
-  done: string;
-  createdAt: string;
+  done: boolean;
+  createdAt: number;
 };
 
 export type PaginatedResponse<T> = {
@@ -71,10 +71,10 @@ export function fetchTasks({
   return fetch(
     `http://localhost:3001/tasks?_page=${page}&_per_page=${per_page}&_sort=${
       sort.createdAt === "asc" ? "createdAt" : "-createdAt"
-    }&userId=${filters?.userId}&title=${filters?.title}`
+    }&userId=${filters?.userId}${filters?.title ? `&title=${filters.title}` : ""}`
   )
-  .then((res) => res.json() as Promise<PaginatedResponse<Task>>)
-  .then((r) => ({ ...r, page }));
+    .then((res) => res.json())
+    .then((r) => ({ ...r, page }));
 }
 
 export function createTask(task: Omit<Task, "id" | "createdAt">) {
